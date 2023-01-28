a65bab143d6f8a7c610d4d54858ce142dd64fd54
szenasi.b <szenasi.peter@verebelyszki.hu>
2023-01-28 18:52:47 +0100

    save

--- szamkitalalos.py
+++ szamkitalalos.py
@@ -1,3 +1,15 @@
 import random
 
-print('fukgz')
+szam = random.randint(1, 100)
+print(szam)
+tipp = int(input('Írj be egy számot 1 és 100 között: '))
+
+while tipp != szam:
+    if tipp > szam:
+        print('Túl magas!')
+    else:
+        print('Túl alacsony')
+    print(szam)
+    tipp = int(input('Írj be egy számot 1 és 100 között: '))
+
+print(f'Gratulálok, eltaláltad! A szám {szam} volt. ')
